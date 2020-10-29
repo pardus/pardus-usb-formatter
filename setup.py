4cda4f068af106d0bbda39fe478b6d7e402b22c4
Emin Fedar <eminfedar@gmail.com>
2020-10-29 19:27:52 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@
 from setuptools import setup, find_packages
 
 data_files = [
-    ("/usr/share/applications/", ["tr.org.pardus.image-writer.desktop"]),
-    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-image-writer.mo"]),
-    ("/usr/share/pardus/pardus-image-writer/", ["icon.svg", "main.svg"]),
-    ("/usr/share/pardus/pardus-image-writer/src", ["src/main.py", "src/MainWindow.py", "src/ImageWriter.py", "src/USBDeviceManager.py"]),
-    ("/usr/share/pardus/pardus-image-writer/ui", ["ui/MainWindow.glade"]),
-    ("/usr/share/polkit-1/actions", ["tr.org.pardus.pkexec.pardus-image-writer.policy"]),
-    ("/usr/bin/", ["pardus-image-writer"])
+    ("/usr/share/applications/", ["tr.org.pardus.usb-formatter.desktop"]),
+    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-usb-formatter.mo"]),
+    ("/usr/share/pardus/pardus-usb-formatter/", ["icon.svg", "main.svg"]),
+    ("/usr/share/pardus/pardus-usb-formatter/src", ["src/main.py", "src/MainWindow.py", "src/USBFormatter.py", "src/USBDeviceManager.py"]),
+    ("/usr/share/pardus/pardus-usb-formatter/ui", ["ui/MainWindow.glade"]),
+    ("/usr/share/polkit-1/actions", ["tr.org.pardus.pkexec.pardus-usb-formatter.policy"]),
+    ("/usr/bin/", ["pardus-usb-formatter"])
 ]
 
 setup(
-    name="Pardus Image Writer",
+    name="Pardus USB Formatter",
     version="0.2.0~Beta1",
     packages=find_packages(),
-    scripts=["pardus-image-writer"],
+    scripts=["pardus-usb-formatter"],
     install_requires=["PyGObject"],
     data_files=data_files,
     author="Emin Fedar",
     author_email="emin.fedar@pardus.org.tr",
-    description="Pardus ISO Image Writer.",
+    description="Pardus USB Formatter.",
     license="GPLv3",
-    keywords="iso usb image burn write",
+    keywords="usb format pardus",
     url="https://www.pardus.org.tr",
 )
