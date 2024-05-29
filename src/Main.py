0673766c35441a4920013f5f3d73749485799369
sulincix <aliriza.keskin@pardus.org.tr>
2024-05-29 11:20:05 +0000

    fix kde wayland wrong icon issue

--- src/Main.py
+++ src/Main.py
@@ -4,7 +4,7 @@ import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gio, Gtk
+from gi.repository import Gio, Gtk, GLib
 
 from MainWindow import MainWindow
 
@@ -14,6 +14,7 @@ class Application(Gtk.Application):
         super().__init__(*args, application_id="tr.org.pardus.usb-formatter", flags=Gio.ApplicationFlags.HANDLES_OPEN,
                          **kwargs)
         self.window = None
+        GLib.set_prgname("tr.org.pardus.usb-formatter")
 
     def do_activate(self):
         self.window = MainWindow(self)
